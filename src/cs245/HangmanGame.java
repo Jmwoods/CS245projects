32eafa16a909100c65cb49f0f6587c100df6bab2
ejm <ejm@b98-wc2-auth-134-71-75-248.wlan.cpp.edu>
2016-09-29 18:30:22 -0700

    Added header for initial file

--- src/cs245/HangmanGame.java
+++ src/cs245/HangmanGame.java
@@ -1,8 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**************************************
+*   file:HangmanGame.java
+*   Authors:
+*   Class: CS 245 - Graphical User Interface
+* 
+*   Assignment: Point and Click game v1.0
+*   Date Last Modified: 
+*   Purpose:
+* 
+***************************************/
 package cs245;
 
 import java.awt.BorderLayout;
