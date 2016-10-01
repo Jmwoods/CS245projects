d5aaeb10c7c3de13ddc62bca2c9133d0dd87e5e4
ejm <ejm@b98-wc2-auth-134-71-75-248.wlan.cpp.edu>
2016-09-30 23:46:39 -0700

    Rough Start Screen

--- src/cs245/HangmanGUI.java
+++ src/cs245/HangmanGUI.java
@@ -26,16 +26,23 @@ public class HangmanGUI extends JFrame{
     public HangmanGUI(){
         super("CS 245 Hangman");
         initializeWindow();
+        startScreen();
+
+        }
+    private void startScreen(){
         title = new JLabel("CS 245 Hangman v1.0", JLabel.CENTER);
         title.setFont(new Font("Comic Sans MS",Font.BOLD, 36));
+        title.setForeground(Color.white);
         teamName = new JLabel("Team: Default_Username", JLabel.CENTER);
         teamName.setFont(new Font("Comic Sans MS",Font.BOLD, 16));
+        teamName.setForeground(Color.white);
         this.add(title, BorderLayout.NORTH);
         this.add(teamName, BorderLayout.CENTER);
-      
+              
     }
 
     private void initializeWindow(){
+        this.getContentPane().setBackground(Color.BLACK);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WINDOW_LENGTH, WINDOW_WIDTH);
         this.setResizable(false);
