5982f2cd160757e742bbade798a82e74c8158585
ejm <ejm@b98-wc2-auth-134-71-75-248.wlan.cpp.edu>
2016-10-01 21:31:25 -0700

    Rough Start Screen

--- src/cs245/HangmanGUI.java
+++ src/cs245/HangmanGUI.java
@@ -8,12 +8,14 @@ package cs245;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
  * @author ejm
  */
-public class HangmanGUI extends JFrame{
+public class HangmanGUI extends JFrame implements ActionListener{
     
     private static final int WINDOW_LENGTH = 600;
     
@@ -23,23 +25,42 @@ public class HangmanGUI extends JFrame{
     
     private JButton play, highScore, credits, back, skip;
     
+    private JPanel mainFrame, gameFrame, letterFrame;
+    
+    private Timer timer;
+    
     public HangmanGUI(){
         super("CS 245 Hangman");
         initializeWindow();
         startScreen();
-
-        }
+        mainMenu();
+               
+    }
     private void startScreen(){
         title = new JLabel("CS 245 Hangman v1.0", JLabel.CENTER);
-        title.setFont(new Font("Comic Sans MS",Font.BOLD, 36));
+        title.setFont(new Font("Comic Sans MS",Font.BOLD, 42));
         title.setForeground(Color.white);
         teamName = new JLabel("Team: Default_Username", JLabel.CENTER);
-        teamName.setFont(new Font("Comic Sans MS",Font.BOLD, 16));
+        teamName.setFont(new Font("Comic Sans MS",Font.BOLD, 20));
         teamName.setForeground(Color.white);
-        this.add(title, BorderLayout.NORTH);
+        this.add(title, BorderLayout.PAGE_START);
         this.add(teamName, BorderLayout.CENTER);
-        
-              
+    }
+    
+    private void mainMenu(){
+        JPanel right = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        //right.setBackground(Color.BLACK);
+        mainFrame = new JPanel(new GridLayout(3,1));
+        right.add(mainFrame);
+        mainFrame.setBackground(Color.black);
+        //mainFrame.setOpaque(false);
+        play = new JButton("Play");
+        highScore = new JButton("High Score");
+        credits = new JButton("Credits");
+        mainFrame.add(play);
+        mainFrame.add(credits);
+        mainFrame.add(highScore);
+        this.add(right, BorderLayout.PAGE_END);   
     }
 
     private void initializeWindow(){
@@ -48,12 +69,12 @@ public class HangmanGUI extends JFrame{
         this.setSize(WINDOW_LENGTH, WINDOW_WIDTH);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
+        this.setLayout(new BorderLayout(0,100));
         this.setVisible(true);
-        this.setLayout(new BorderLayout());
     }
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object object = e.getSource();
+    }
     
 }
