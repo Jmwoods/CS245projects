06687d06fd45da321f3b1c2fc81ede29a8ee5a31
ejm <ejm@b98-wc2-auth-134-71-75-248.wlan.cpp.edu>
2016-10-02 16:22:51 -0700

    GUI elements added except main game and high scores

--- src/cs245/HangmanGUI.java
+++ src/cs245/HangmanGUI.java
@@ -1,8 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**************************************
+*   file:HangmanGUI.java
+*   @author Ervin Maulas, Joel Woods, Jose Garcia, Alan Chen
+*   Class: CS 245 - Graphical User Interface
+* 
+*   Assignment: Point and Click game 
+*   @version v1.0
+*   Date Last Modified: 1 October 2016
+*   Purpose: Creates the GUI elements that the user will interact with
+* 
+***************************************/
 package cs245;
 
 import java.awt.*;
@@ -11,66 +17,113 @@ import javax.swing.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- *
- * @author ejm
- */
 public class HangmanGUI extends JFrame implements ActionListener{
     
     private static final int WINDOW_LENGTH = 600;
     
     private static final int WINDOW_WIDTH = 400;
     
-    private JLabel title, teamName;
+    private JLabel title, teamName, creditLabel, name1, name2, name3, name4;
     
     private JButton play, highScore, credits, back, skip;
     
-    private JPanel mainFrame, gameFrame, letterFrame;
+    private JPanel mainPanel, menuPanel, gamePanel, letterPanel, creditsMainPanel,
+            creditsPanel;
     
     private Timer timer;
-    
+    /**
+     * Creates the objects needed for the game to reach the main menu.
+     */
     public HangmanGUI(){
         super("CS 245 Hangman");
         initializeWindow();
         startScreen();
-        mainMenu();
+        //mainMenu();
+        //creditScreen();
+        
                
     }
+    /**
+     * Creates the objects needed to display the initial start screen, and adds
+     * them to the frame.
+     */
     private void startScreen(){
-        title = new JLabel("CS 245 Hangman v1.0", JLabel.CENTER);
-        title.setFont(new Font("Comic Sans MS",Font.BOLD, 42));
-        title.setForeground(Color.white);
-        teamName = new JLabel("Team: Default_Username", JLabel.CENTER);
-        teamName.setFont(new Font("Comic Sans MS",Font.BOLD, 20));
-        teamName.setForeground(Color.white);
-        this.add(title, BorderLayout.PAGE_START);
-        this.add(teamName, BorderLayout.CENTER);
+        
+        title = createLabel("CS 245 Hangman v1.0", 42, Color.white);
+        teamName = createLabel("Team: Default_Username", 20, Color.white);
+        mainPanel.add(title, BorderLayout.PAGE_START);
+        mainPanel.add(teamName, BorderLayout.CENTER);
     }
-    
+    /**
+     * Creates the objects needed to display the main menu, and adds them to the
+     * frame.
+     */
     private void mainMenu(){
-        JPanel right = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        //right.setBackground(Color.BLACK);
-        mainFrame = new JPanel(new GridLayout(3,1));
-        right.add(mainFrame);
-        mainFrame.setBackground(Color.black);
-        //mainFrame.setOpaque(false);
+        JPanel right = new JPanel(new GridLayout(2,1));
+        right.setOpaque(false);
+        menuPanel = new JPanel(new GridLayout(3,1));
+        JPanel test = new JPanel();
+        test.setOpaque(false);
+        right.add(test);
+        right.add(menuPanel);
+        menuPanel.setOpaque(false);
         play = new JButton("Play");
         highScore = new JButton("High Score");
         credits = new JButton("Credits");
-        mainFrame.add(play);
-        mainFrame.add(credits);
-        mainFrame.add(highScore);
-        this.add(right, BorderLayout.PAGE_END);   
+        menuPanel.add(play);
+        menuPanel.add(credits);
+        menuPanel.add(highScore);
+        this.add(right, BorderLayout.LINE_END);   
     }
-
+    /**
+     * Will create objects needed to show the credit screen.
+     */
+    
+    private void creditScreen(){
+        creditsMainPanel = new JPanel(new BorderLayout());
+        JPanel left = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        back = new JButton("back");
+        left.add(back);
+        creditsMainPanel.add(left, BorderLayout.PAGE_END);
+        creditsPanel = new JPanel(new GridLayout(5,1));
+        creditsMainPanel.add(creditsPanel, BorderLayout.CENTER);
+        creditLabel = createLabel("Credits", 46, Color.red);
+        creditsPanel.add(creditLabel);
+        name1 = createLabel("Ervin Maulas, 008873462", 22, Color.BLACK);
+        creditsPanel.add(name1);
+        name2 = createLabel("Joel Woods, 000000000", 22, Color.BLACK);
+        creditsPanel.add(name2);
+        name3 = createLabel("Jose Garcia, 000000000", 22, Color.BLACK);
+        creditsPanel.add(name3);
+        name4 = createLabel ("Alan Chen, 000000000", 22, Color.BLACK);
+        creditsPanel.add(name4);
+        add(creditsMainPanel);      
+    }
+    /**
+     * Adds the attributes for the JFrame created for the game.
+     */
     private void initializeWindow(){
-        this.getContentPane().setBackground(Color.BLACK);
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(Color.BLACK);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WINDOW_LENGTH, WINDOW_WIDTH);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-        this.setLayout(new BorderLayout(0,100));
         this.setVisible(true);
+        this.add(mainPanel);
+    }
+    /**
+     * Creates the specific labels needed for the hangman game
+     * @param text text to be displayed by the label
+     * @param size the size of the text for the label
+     * @param color color of the text for the label
+     * @return label object with correct settings for game elements
+     */
+    private JLabel createLabel(String text, int size, Color color){
+        JLabel tempLabel = new JLabel(text, JLabel.CENTER);
+        tempLabel.setFont(new Font("Comic Sans MS",Font.BOLD, size));
+        tempLabel.setForeground(color);
+        return tempLabel;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
