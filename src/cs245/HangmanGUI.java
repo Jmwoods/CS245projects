22942d731c63741d11fefacdc5cc61bd2d290be8
ejm <ejm@b98-wc2-auth-134-71-75-248.wlan.cpp.edu>
2016-10-02 20:26:15 -0700

    Menus and Buttons work. Still need to implement actual game GUI, game backend, and three second appearance of the title screen

--- src/cs245/HangmanGUI.java
+++ src/cs245/HangmanGUI.java
@@ -13,6 +13,8 @@ package cs245;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Set;
 import javax.swing.*;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -27,8 +29,8 @@ public class HangmanGUI extends JFrame implements ActionListener{
     
     private JButton play, highScore, credits, back, skip;
     
-    private JPanel mainPanel, menuPanel, gamePanel, letterPanel, creditsMainPanel,
-            creditsPanel;
+    private JPanel mainPanel, menuPanel, gamePanel, letterPanel, displayMainPanel,
+            displayPanel;
     
     private Timer timer;
     /**
@@ -37,28 +39,28 @@ public class HangmanGUI extends JFrame implements ActionListener{
     public HangmanGUI(){
         super("CS 245 Hangman");
         initializeWindow();
-        startScreen();
-        //mainMenu();
-        //creditScreen();
-        
-               
+        mainMenu();          
     }
     /**
      * Creates the objects needed to display the initial start screen, and adds
      * them to the frame.
      */
-    private void startScreen(){
-        
+    private void startScreen(){       
         title = createLabel("CS 245 Hangman v1.0", 42, Color.white);
         teamName = createLabel("Team: Default_Username", 20, Color.white);
-        mainPanel.add(title, BorderLayout.PAGE_START);
-        mainPanel.add(teamName, BorderLayout.CENTER);
+        this.add(title, BorderLayout.PAGE_START);
+        this.add(teamName, BorderLayout.CENTER);
     }
     /**
      * Creates the objects needed to display the main menu, and adds them to the
      * frame.
+     * @TODO Change GridLayout to BoxLayout
      */
     private void mainMenu(){
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBackground(Color.BLACK);
+        this.add(mainPanel);
+        
         JPanel right = new JPanel(new GridLayout(2,1));
         right.setOpaque(false);
         menuPanel = new JPanel(new GridLayout(3,1));
@@ -68,49 +70,74 @@ public class HangmanGUI extends JFrame implements ActionListener{
         right.add(menuPanel);
         menuPanel.setOpaque(false);
         play = new JButton("Play");
+        play.addActionListener(this);
         highScore = new JButton("High Score");
+        highScore.addActionListener(this);
         credits = new JButton("Credits");
+        credits.addActionListener(this);
         menuPanel.add(play);
         menuPanel.add(credits);
         menuPanel.add(highScore);
-        this.add(right, BorderLayout.LINE_END);   
+        mainPanel.add(right, BorderLayout.LINE_END);   
     }
     /**
      * Will create objects needed to show the credit screen.
      */
     
-    private void creditScreen(){
-        creditsMainPanel = new JPanel(new BorderLayout());
+    private void creditsOrHighScoreScreen(boolean isCredits){
+        displayMainPanel = new JPanel(new BorderLayout());
         JPanel left = new JPanel(new FlowLayout(FlowLayout.LEFT));
         back = new JButton("back");
+        back.addActionListener(this);
         left.add(back);
-        creditsMainPanel.add(left, BorderLayout.PAGE_END);
-        creditsPanel = new JPanel(new GridLayout(5,1));
-        creditsMainPanel.add(creditsPanel, BorderLayout.CENTER);
-        creditLabel = createLabel("Credits", 46, Color.red);
-        creditsPanel.add(creditLabel);
-        name1 = createLabel("Ervin Maulas, 008873462", 22, Color.BLACK);
-        creditsPanel.add(name1);
-        name2 = createLabel("Joel Woods, 000000000", 22, Color.BLACK);
-        creditsPanel.add(name2);
-        name3 = createLabel("Jose Garcia, 000000000", 22, Color.BLACK);
-        creditsPanel.add(name3);
-        name4 = createLabel ("Alan Chen, 000000000", 22, Color.BLACK);
-        creditsPanel.add(name4);
-        add(creditsMainPanel);      
+        displayMainPanel.add(left, BorderLayout.PAGE_END);
+        creditsOrHighScore(isCredits);
+        this.add(displayMainPanel);
+    }
+    /**
+     * Populates a generic panel with back button with either the credits or 
+     * high score.
+     * @param isCredits stores boolean which decides which elements to populate
+     * panel with
+     * @TODO Change GridLayout to BoxLayout
+     */
+    private void creditsOrHighScore(boolean isCredits){
+        if(isCredits){
+            displayPanel = new JPanel(new GridLayout(5,1));
+            creditLabel = createLabel("Credits", 46, Color.red);
+            displayPanel.add(creditLabel);
+            name1 = createLabel("Ervin Maulas, 008873462", 22, Color.BLACK);
+            displayPanel.add(name1);
+            name2 = createLabel("Joel Woods, 000000000", 22, Color.BLACK);
+            displayPanel.add(name2);
+            name3 = createLabel("Jose Garcia, 000000000", 22, Color.BLACK);
+            displayPanel.add(name3);
+            name4 = createLabel ("Alan Chen, 000000000", 22, Color.BLACK);
+            displayPanel.add(name4);            
+        }
+        else{
+            displayPanel = new JPanel(new GridLayout(6,1));
+            displayPanel.add(createLabel("High Scores", 46, Color.GREEN));
+            for(int i = 0; i < 5; i++){
+                displayPanel.add(createLabel("ABC.....000", 22, Color.BLACK));   
+            }
+            
+        }
+        displayMainPanel.add(displayPanel, BorderLayout.CENTER);
     }
     /**
      * Adds the attributes for the JFrame created for the game.
      */
     private void initializeWindow(){
-        mainPanel = new JPanel(new BorderLayout());
-        mainPanel.setBackground(Color.BLACK);
+//        mainPanel = new JPanel(new BorderLayout());
+//        mainPanel.setBackground(Color.BLACK);
+        getContentPane().setBackground(Color.BLACK);       
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WINDOW_LENGTH, WINDOW_WIDTH);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        this.add(mainPanel);
+//        this.add(mainPanel);
     }
     /**
      * Creates the specific labels needed for the hangman game
@@ -128,6 +155,31 @@ public class HangmanGUI extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         Object object = e.getSource();
+        if(object == back){
+            getContentPane().remove(displayMainPanel);
+            mainMenu();
+            getContentPane().validate();
+            getContentPane().repaint();
+        }
+        else if(object == play){
+            //do nothing
+        }
+        else if(object == highScore){
+            getContentPane().remove(mainPanel);
+            creditsOrHighScoreScreen(false);
+            getContentPane().validate();
+            getContentPane().repaint();
+        }
+        else if(object == credits){
+            getContentPane().remove(mainPanel);
+            creditsOrHighScoreScreen(true);
+            getContentPane().validate();
+            getContentPane().repaint();
+
+        }
+        else{
+            //do nothing
+        }
     }
     
 }
